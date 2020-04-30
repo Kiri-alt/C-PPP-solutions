434ec6708f0daf5f2d8270ac982ae383f6aead63
Gijs van de Linde <gvandelinde10@gmail.com>
2020-04-30 14:01:18 +0200

    New Exercise

---
+++ Chapter 4/Ch.4_E.8_Base2_Rice.cpp
@@ -0,0 +1,31 @@
+// The inventor asked for one grain of rice for the first square, 2 for the
+// second, 4 for the third, and so on, doubling for each of the 64 squares.
+
+#include "../../std_lib_facilities.h"
+
+int count_steps(int minimum, int iteration);
+
+int main()
+{
+    // i'm first going to try to solve this issue with recursion.
+    int local_minimum = 10000; // minimal amount of rice grains asked
+    int minimum_squares = count_steps(local_minimum, 0);
+    cout << "\nThe minimum amound of squares you'd need for that amount of rice is: " << minimum_squares << endl;
+}
+
+int count_steps(int minimum, int iteration)
+{
+    if (minimum < pow(2, iteration)) return iteration;
+    else
+    {
+        // output for clarity
+        cout << "> i = " << iteration << ", bigger than: " << pow(2, iteration) << endl;
+
+        // up the iteration
+        ++iteration;
+        
+        // call the function on the new iteration
+        return count_steps(minimum, iteration);
+    }
+    return 0;
+}
