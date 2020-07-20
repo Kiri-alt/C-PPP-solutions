cd587c000099fad22d25f0f1525a5a21aff79b53
G <62843594+Kiri-alt@users.noreply.github.com>
2020-07-20 18:07:09 +0200

    Add comments

--- Chapter 4/Ch.4_E.2_Median.cpp
+++ Chapter 4/Ch.4_E.2_Median.cpp
@@ -4,13 +4,18 @@ double median(vector<double> list);
 
 int main()
 {
+	// load vector with test values
 	vector<double> vector_test = { 1.2, 1.3, 1.4, 2.1 };
+	
+	// output result of median function on test-vector 
 	cout << median(vector_test);
 }
 
 double median(vector<double> list) 
 {
+	// index of (lower) middle item in vector
 	int spot = list.size() / 2 - 1;
+	
 	if (list.size() % 2 == 0)
 	{
 		return (list[spot] + list[spot++]) / 2;
